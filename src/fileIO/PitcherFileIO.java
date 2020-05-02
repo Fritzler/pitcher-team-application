1c9d501b0334771c3a4e321d827cc29d6c9d24b0
Fritz <37812303+Fritzler@users.noreply.github.com>
2020-05-01 20:09:46 -0400

    Update PitcherFileIO.java

--- src/fileIO/PitcherFileIO.java
+++ src/fileIO/PitcherFileIO.java
@@ -7,6 +7,7 @@
  * Revision: April 29, 2020
  * Made OutputToFile() process a date for the filename
 */
+
 package fileIO;
 
 import java.io.*;
@@ -17,7 +18,7 @@ public class PitcherFileIO {
     
     public void OutputToFile(Pitcher p){
         // Set the path of the .txt file to the same as the java file
-        Path pitcherFilePath = Paths.get("pitchers.txt");
+        Path pitcherFilePath = Paths.get(p.getFilename());
         
         // Attempt to prevent a FileNotFoundException
         if (Files.exists(pitcherFilePath)) {
@@ -38,12 +39,10 @@ public class PitcherFileIO {
                 out.print(p.getStrikeOuts() + "\t");
                 out.print(p.getAtBats() + "\t");
                 out.print(p.getBattersFaced() + "\t");
-                out.println(p.getNumberPitches() + "\t");
-                out.print(p.getGameDate()); // Final entry, no \t,
+                out.println(p.getNumberPitches()); // Final entry, no \t,
 
                 // Close the file
                 out.close();
-                System.out.println("Added to the file successfully.");
             } catch (IOException e) {
                  System.out.println("Error on writing to file: " + e);
             }           
