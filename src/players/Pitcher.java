508e5e57f3126902f4e6a660d564f8de485bd06b
kohne8604 <54292181+kohne8604@users.noreply.github.com>
2020-05-07 17:50:37 -0400

    "add" functions, ERA tracking

--- src/players/Pitcher.java
+++ src/players/Pitcher.java
@@ -8,6 +8,9 @@ package players;
  * Changelog:
  * Revision: April 25, 2020
  * Fixed Data Type of inningsPitched
+
+ * Revision: May 7, 2020
+ * Added new constructor, "add" functions for game summaries, ERA tracking
 */
 public class Pitcher {
     
@@ -23,6 +26,7 @@ public class Pitcher {
     private int atBats;
     private int battersFaced;
     private int numberPitches;
+    private double ERA;
     private String filename;
     
     // constructor
@@ -39,6 +43,21 @@ public class Pitcher {
         this.battersFaced = bf;
         this.numberPitches = np;
         this.filename = fn;
+    }
+    
+    // Constructor for the sake of summaries, initalize everything at 0
+    public Pitcher(String name){
+        this.playerName = name;
+        this.inningsPitched = 0.0;
+        this.baseHits = 0;
+        this.runsScored = 0;
+        this.earnedRuns = 0;
+        this.walksAllowed = 0;
+        this.strikeOuts = 0;
+        this.atBats = 0;
+        this.battersFaced = 0;
+        this.numberPitches = 0;
+        this.ERA = 0;
     }
     
 
@@ -59,8 +78,12 @@ public class Pitcher {
         return inningsPitched;
     }
 
-    public void setInningsPitched(int inningsPitched) {
+    public void setInningsPitched(double inningsPitched) {
         this.inningsPitched = inningsPitched;
+    }
+    
+    public void addInningsPitched(double inningsPitched) {
+        this.inningsPitched += inningsPitched;
     }
 
     // Base Hits (H)
@@ -70,6 +93,10 @@ public class Pitcher {
 
     public void setBaseHits(int baseHits) {
         this.baseHits = baseHits;
+    }
+    
+    public void addBaseHits(int baseHits) {
+        this.baseHits += baseHits;
     }
 
     // Runs Scored (R)
@@ -81,6 +108,10 @@ public class Pitcher {
         this.runsScored = runsScored;
     }
     
+    public void addRunsScored(int runsScored) {
+        this.runsScored += runsScored;
+    }
+    
     // Earned Runs (ER)
     public int getEarnedRuns() {
         return earnedRuns;
@@ -90,6 +121,10 @@ public class Pitcher {
         this.earnedRuns = earnedRuns;
     }
 
+    public void addEarnedRuns(int earnedRuns) {
+        this.earnedRuns += earnedRuns;
+    }
+    
     // Walks Allowed (BB)
     public int getWalksAllowed() {
         return walksAllowed;
@@ -99,6 +134,10 @@ public class Pitcher {
         this.walksAllowed = walksAllowed;
     }
 
+    public void addWalksAllowed(int walksAllowed) {
+        this.walksAllowed += walksAllowed;
+    }
+    
     // Strike Outs (SO)
     public int getStrikeOuts() {
         return strikeOuts;
@@ -106,6 +145,10 @@ public class Pitcher {
 
     public void setStrikeOuts(int strikeOuts) {
         this.strikeOuts = strikeOuts;
+    }
+    
+    public void addStrikeOuts(int strikeOuts) {
+        this.strikeOuts += strikeOuts;
     }
 
     // At-Bats (AB)
@@ -116,6 +159,10 @@ public class Pitcher {
     public void setAtBats(int atBats) {
         this.atBats = atBats;
     }
+    
+    public void addAtBats(int atBats) {
+        this.atBats += atBats;
+    }
 
     // Batters Faced (BF)
     public int getBattersFaced() {
@@ -125,6 +172,10 @@ public class Pitcher {
     public void setBattersFaced(int battersFaced) {
         this.battersFaced = battersFaced;
     }
+    
+    public void addBattersFaced(int battersFaced) {
+        this.battersFaced += battersFaced;
+    }
 
     // Number of Pitches (NP)
     public int getNumberPitches() {
@@ -133,6 +184,19 @@ public class Pitcher {
 
     public void setNumberPitches(int numberPitches) {
         this.numberPitches = numberPitches;
+    }
+    
+    public void addNumberPitches(int numberPitches) {
+        this.numberPitches += numberPitches;
+    }
+    
+    // Earned Run Average (only used in summary report)
+    public double getERA() {
+        return ERA;
+    }
+    
+    public void addERA(double ERA){
+        this.ERA += ERA;
     }
     
     // Name of file to write to
